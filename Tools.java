5c6e015771aafe0e030d4af6f4c703934ef22ef0
EDDY, JONATHAN LEE (Jonathan) <johnnyeddy.je@gmail.com>
2021-05-10 13:23:05 +1200

    Tools: Method for calculating the index of coincidence

--- Tools.java
+++ Tools.java
@@ -1,9 +1,18 @@
 
-
+import java.io.*;
+import java.util.*;
 
 public class Tools {
 
-
+    public static void main(String[] args) {
+        try {
+            // testing methods
+            // System.out.println(indexOfCoincidence(args[0]));
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
     // removes spaces from a string
     public static String removeSpaces(String message) {
@@ -18,6 +27,23 @@ public class Tools {
             result += s;
         }
         return result.toLowerCase();
+    }
+
+
+    public static double indexOfCoincidence(String message) {
+        message = simplifyMessage(message);
+        Hashtable<Character, Integer> dictionary = new Hashtable<Character, Integer>();
+        // count occurences of characters
+        for (char c: message.toCharArray()) {
+            dictionary.put(c, (dictionary.get(c) != null ? dictionary.get(c) : 0) + 1);
+        }
+        double output = 0;
+        int n = message.length();
+        int devisor = n * (n - 1);
+        for (Integer count: dictionary.values()) {
+            output += (double)count * (count - 1) / devisor;
+        }
+        return output;
     }
 
 }
