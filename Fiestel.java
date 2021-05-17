f2e0a2e03f21639cf5a1311161739ae5f80061fa
TheSecretWalrus <anthony.scott.wilson@gmail.com>
2021-05-17 12:37:46 +1200

    Cleaned up code for Fiestel cipher

--- Fiestel.java
+++ Fiestel.java
@@ -3,6 +3,9 @@ public class Fiestel {
     //key is in the form of a decimal number each digit representing a function (0-9)
     //recommended key length is 20+ digits
 
+    /*
+    Pretty self explanatory
+     */
     public static String encrypt(String m, String k){
 
         //convert message into bits
@@ -20,24 +23,76 @@ public class Fiestel {
 
         int[] keySchedule = new int[k.length()];
 
-        //just assume string entered is a hex num for now and create a check later
+        try{
+            //parsing key String
+            for(int i = 0; i < keySchedule.length; i++){
+                keySchedule[i] = Integer.parseInt(k.substring(i,i+1));
+            }
+        }
+        catch(Exception e){ //bad key scenario
+            return "Please enter the key for the Fiestel cipher as a decimal number \n Key entered:" + k;
+        }
 
-        //get key schedule from master key
+        boolean[] temp;
+
+        //loops through the steps for encryption
         for(int i = 0; i < keySchedule.length; i++){
-            keySchedule[i] = Integer.parseInt(k.substring(i,i+1));
+            temp = arrayCopy(rightM);
+            rightM = stepCalc(leftM, rightM, keySchedule[i]);
+            leftM = temp;
+        }
+
+        //reconstructs bit array again
+        //also swaps left and right as final step
+        for(int i = 0; i < messageBits.length/2; i++){
+            messageBits[i] = rightM[i];
+            messageBits[i + messageBits.length/2] = leftM[i];
+        }
+
+        return BitArrayToString(messageBits);
+    }
+
+    /*
+    Basically an exact copy of encrypt with some minor changes
+    Honestly this cipher is kind of nuts how encryption/decryption work together
+     */
+    public static String decrypt(String m, String k){
+        //convert message into bits
+        boolean[] messageBits = StringToBitArray(m);
+
+        //convert to left and right arrays
+        boolean[] leftM = new boolean[messageBits.length/2];
+        boolean[] rightM = new boolean[messageBits.length/2];
+
+        for(int i = 0; i < messageBits.length/2; i++){
+            rightM[i] = messageBits[i];
+            leftM[i] = messageBits[i + messageBits.length/2];
+        }
+
+        //getting key schedule from key
+        int[] keySchedule = new int[k.length()];
+
+        try{
+            //parsing key String
+            for(int i = 0; i < keySchedule.length; i++){
+                keySchedule[i] = Integer.parseInt(k.substring(i,i+1));
+            }
+        }
+        catch(Exception e){ //bad key scenario
+            return "Please enter the key for the Fiestel cipher as a decimal number \n Key entered:" + k;
         }
 
         boolean[] temp;
 
         for(int i = 0; i < keySchedule.length; i++){
-            temp = arrayCopy(rightM);
-            rightM = stepCalc(leftM, rightM, keySchedule[i]); //this line here is causing the trouble
-            leftM = temp;
+            temp = arrayCopy(leftM);
+            leftM = stepCalc(rightM, leftM, keySchedule[keySchedule.length-1-i]);
+            rightM = temp;
         }
 
         for(int i = 0; i < messageBits.length/2; i++){
-            messageBits[i] = rightM[i];
-            messageBits[i + messageBits.length/2] = leftM[i];
+            messageBits[i] = leftM[i];
+            messageBits[i + messageBits.length/2] = rightM[i];
         }
 
         return BitArrayToString(messageBits);
@@ -83,22 +138,8 @@ public class Fiestel {
                 a = arrayCopy(rightM);
         }
 
-        //boolean[] a = func1(rightM);
-
-        System.out.println("");
-
         for(int i = 0; i < rightM.length; i++){ //xor with func(rightM) and leftM
             a[i] = leftM[i] ^ a[i];
-
-            if(i % 8 == 0){
-                System.out.println("");
-            }
-            if(a[i]){
-                System.out.print("1");
-            }
-            else{
-                System.out.print("0");
-            }
         }
         return a;
     }
@@ -208,34 +249,28 @@ public class Fiestel {
      */
 
     /*
+    converts a string into a boolean array containing all the chars as ascii binary
     IMPORTANT NOTE: removes new line from end of string
      */
     private static boolean[] StringToBitArray(String m){
 
-        m = m.substring(0,m.length()-1);
-
-        System.out.println(m);
-        System.out.println(m.length());
+        m = m.substring(0,m.length()-1); //removing new line char
 
         int arrayIndex = 0; //index for counting where in the bit array we are
         boolean[] StringBits = new boolean[m.length() * 8]; //all the bits
         for(char c : m.toCharArray()){
 
-            //System.out.println(c);
-
             int temp = c;
-            //loops through the bits of an individual character, extracts then and puts them in into the boolean array
+            //loops through the bits of an individual character, extracts then and puts them into the boolean array
 
             for(int i = 7; i >= 0; i--){ //count from 7 to 0
                 //this operation takes the next bit of the character
                 temp = (c >> i) & 0x1;
                 if(temp == 0){
                     StringBits[arrayIndex] = false;
-                    //System.out.println("0");
                 }
                 else if(temp == 1){
                     StringBits[arrayIndex] = true;
-                    //System.out.println("1");
                 }
                 else{
                     System.out.println("something has gone horribly wrong with the bit conversion");
@@ -244,22 +279,12 @@ public class Fiestel {
             }
         }
 
-        //testing
-        for(int i = 0; i < StringBits.length; i++){
-            if(i % 8 == 0){
-                System.out.println("");
-            }
-            if(StringBits[i]){
-                System.out.print("1");
-            }
-            else{
-                System.out.print("0");
-            }
-        }
-
         return StringBits;
     }
 
+    /*
+    converts a boolean array of ascii chars back into a string
+     */
     private static String BitArrayToString(boolean[] m){
 
         if((m.length / 8) * 8 != m.length){
@@ -267,15 +292,9 @@ public class Fiestel {
             return null;
         }
 
-        System.out.println((m.length / 8) * 8 != m.length);
-
         int c = 0; //the char to add to the message string
         String message = "";
         int charBitCount = 0; //cycles from 0 to 7 to count the bit position in the new char
-
-        int sum = 0;
-
-        System.out.println(m.length);
 
         for(int i = 0; i < m.length; i++){
             if(m[i]){ // == 1
@@ -286,15 +305,13 @@ public class Fiestel {
                 charBitCount = 0;
                 message += (char)c;
                 c = 0;
-                sum++;
+                //sum++;
             }
             else{
                 c = c * 2;
                 charBitCount++;
             }
         }
-
-        System.out.println(sum);
 
         return message;
     }
@@ -309,50 +326,7 @@ public class Fiestel {
         return b;
     }
 
-    //basically an exact copy of encrypt with some minor changes
-    //honestly this cipher is kind of nuts how encryption/decryption work together
-    public static String decrypt(String m, String k){
-        //convert message into bits
-        boolean[] messageBits = StringToBitArray(m);
-
-        //convert to left and right arrays
-        //could do a slightly more efficient thing here but it's not really super relevant atm
-        boolean[] leftM = new boolean[messageBits.length/2];
-        boolean[] rightM = new boolean[messageBits.length/2];
-
-        for(int i = 0; i < messageBits.length/2; i++){
-            rightM[i] = messageBits[i];
-            leftM[i] = messageBits[i + messageBits.length/2];
-        }
-
-        int[] keySchedule = new int[k.length()];
-
-        //just assume string entered is a hex num for now and create a check later
-
-        //get key schedule from master key
-        for(int i = 0; i < keySchedule.length; i++){
-            keySchedule[i] = Integer.parseInt(k.substring(i,i+1));
-        }
-
-        //System.out.println(keySchedule[3]);
-
-        boolean[] temp;
-
-        for(int i = 0; i < keySchedule.length; i++){
-            temp = arrayCopy(leftM);
-            leftM = stepCalc(rightM, leftM, keySchedule[keySchedule.length-1-i]);
-            rightM = temp;
-        }
-
-        for(int i = 0; i < messageBits.length/2; i++){
-            messageBits[i] = leftM[i];
-            messageBits[i + messageBits.length/2] = rightM[i];
-        }
-
-        return BitArrayToString(messageBits);
-    }
-
-    //generates a random key 16 hex digits long
+    //generates a random key 20 digits long
     public int genRandomKey(){
 
         int key = (int)(Math.random() * 10);
@@ -364,4 +338,29 @@ public class Fiestel {
 
         return key;
     }
+
+    /*
+    This method attempts to figure out the key for a given message string 'm'
+    I haven't got a clue where to start on this
+     */
+    public static String smartDecrypt(String m, String k){
+        return "";
+    }
+
+    //used for de-bugging
+    //prints out each char in the boolean array as a sequence of 8 bits
+    private static void printBits(boolean[] a){
+        for(int i = 0; i < a.length; i++){
+            if(i % 8 == 0){
+                System.out.println("");
+            }
+            if(a[i]){
+                System.out.print("1");
+            }
+            else{
+                System.out.print("0");
+            }
+        }
+    }
+
 }
